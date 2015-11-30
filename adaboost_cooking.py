e091493a7b6c8ccfb1598af50d3204ff913de31a
Basheer Subei <basheersubei@gmail.com>
2015-11-29 19:10:52 -0600

    added plotting to adaboost script

--- adaboost_cooking.py
+++ adaboost_cooking.py
@@ -22,7 +22,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 # Load in the Data
 # train = pd.read_json('data/train.json')
-train = pd.read_json('data/train3.json')
+train = pd.read_json('data/train1.json')
 
 # Extract the Unique Ingredients
 words = [' '.join(item) for item in train.ingredients]
@@ -33,10 +33,10 @@ vec = CountVectorizer(max_features=2000)
 bag_of_words = vec.fit(words).transform(words).toarray()
 
 ada_clf = AdaBoostClassifier(
-    DecisionTreeClassifier(max_depth=3),
-    n_estimators=600,
+    DecisionTreeClassifier(max_depth=10),
+    n_estimators=500,
     learning_rate=1.5,
-    algorithm="SAMME")
+    algorithm="SAMME.R")
 
 start = time()
 ada_clf.fit(bag_of_words, train.cuisine)
@@ -49,6 +49,21 @@ print("adaboost classifier training in %.2f" % (time() - start))
 
 #print("Estimated accuracy using cross-validation: " , accuracy_score(train.cuisine, train_pred))
 
+
+# use rest of labelled training data to check accuracy score (for plotting)
+test = pd.read_json('data/train2.json')
+test_words = [' '.join(item) for item in test.ingredients]
+test_bag = vec.transform(test_words).toarray()
+test_errors = []
+for test_predict in ada_clf.staged_predict(test_bag):
+    test_errors.append( 1. - accuracy_score(test_predict, test.cuisine) )
+
+plt.figure(figsize=(15, 5))
+
+plt.plot(range(1, len(ada_clf) + 1), test_errors)
+plt.ylabel('Test Error')
+plt.xlabel('Number of Trees')
+plt.show()
 
 # Load in Testing Data
 test = pd.read_json('data/test.json')
